4c631186026fbbabea3d56cfd5f528b19291142e
VVradio <gw@varietyvibesradio.com>
2026-06-14 04:27:44 -0700

    Delete vibes directory

--- vibes/backend/middleware/tiers.js
+++
@@ -1,7 +0,0 @@
-const TIER_LIMITS = {
-  free:  { maxLinks: 5,  maxPhotos: 2  },
-  pro:   { maxLinks: null, maxPhotos: 6  },
-  merch: { maxLinks: null, maxPhotos: 12 },
-};
-
-module.exports = { TIER_LIMITS };
